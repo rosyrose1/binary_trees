446a72d13b895d48419d631615ba428ac764ac4c
Dedeyd70 <dorothyokoto@gmail.com>
2023-10-05 01:56:31 -0700

    binary trees

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,15 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * heap_insert - inserting a value in Max Binary Heap
- * @root: a double pointer to the root node of the Heap
+ * heap_insert - inserts a value in Max Binary Heap
+ * @root: a double pointer to the root node of the Heap to insert the value
  * @value: the value to store in the node to be inserted
- * Return: a pointer to the created node or NULL on failure
+ *
+ * Return: a pointer to the created node
+ * Otherwise NULL on failure
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *tree, *new, *turn;
-	int size, leaves, sub, bit, level, temp;
+	heap_t *tree, *new, *flip;
+	int size, leaf, sub, bit, index, tmp;
 
 	if (!root)
 		return (NULL);
@@ -17,40 +19,41 @@ heap_t *heap_insert(heap_t **root, int value)
 		return (*root = binary_tree_node(NULL, value));
 	tree = *root;
 	size = binary_tree_size(tree);
-	leaves = size;
-	for (level = 0, sub = 1; leaves >= sub; sub *= 2, level++)
-		leaves -= sub;
+	leaf = size;
+	for (index = 0, sub = 1; leaf >= sub; sub *= 2, index++)
+		leaf -= sub;
 
-	for (bit = 1 << (level - 1); bit != 1; bit >>= 1)
-		tree = leaves & bit ? tree->right : tree->left;
-	
-/*
-* Traverse tree to first empty slot based on the binary
-* representation of the number of leaves.
-*/
+	for (bit = 1 << (index - 1); bit != 1; bit >>= 1)
+		tree = leaf & bit ? tree->right : tree->left;
+
 	new = binary_tree_node(tree, value);
-	leaves & 1 ? (tree->right = new) : (tree->left = new);
+	leaf & 1 ? (tree->right = new) : (tree->left = new);
 
-	turn = new;
-	for (; turn->parent && (turn->n > turn->parent->n); turn = turn->parent)
+	flip = new;
+	for (; flip->parent && (flip->n > flip->parent->n); flip = flip->parent)
 	{
-		temp = turn->n;
-		turn->n = turn->parent->n;
-		turn->parent->n = temp;
+		tmp = flip->n;
+		flip->n = flip->parent->n;
+		flip->parent->n = tmp;
 		new = new->parent;
 	}
+
 	return (new);
 }
 
 /**
- * binary_tree_size - calculates the size of a binary tree
- * @tree: tree to calculate.
- * Return: size of the tree, 0 if tree is NULL
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: pointer to the root node of the tree
+ * Return: 0 if tree is NULL, otherwise the size of the tree
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+	size_t left_ht, right_ht;
 
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	if (tree == NULL)
+		return (0);
+	left_ht = binary_tree_size(tree->left);
+	right_ht = binary_tree_size(tree->right);
+
+	return (left_ht + 1 + right_ht);
 }
